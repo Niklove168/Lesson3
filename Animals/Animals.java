4339845c6bce17c70f4296c80f78707464375b56
Niklove168 <Nik210790@gmail.com>
2024-12-09 21:12:23 +0300

    Add files via upload

---
+++ Animals/Animals.java
@@ -0,0 +1,21 @@
+package Lesson3.Animals;
+
+public class Animals {
+    private static int numAnimals;
+
+    public Animals() {
+        numAnimals++;
+    }
+
+    public static int getNumAnimals() {
+        return numAnimals;
+    }
+
+    public void run(int distance) {
+        System.out.println("Animal ran " + distance + " meters");
+    }
+
+    public void swim(int distance) {
+        System.out.println("Animal swam by " + distance + " meters");
+    }
+}
